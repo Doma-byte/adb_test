a9c071ec3753567246bf4ceda891ebe29b1c16a5
Dikshit <singhdikshit69@gmail.com>
2023-10-16 13:56:03 +0530

    Add error handling

--- src/app/src/components/Todo.js
+++ src/app/src/components/Todo.js
@@ -1,4 +1,4 @@
-import React,{useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import "./Todo.css";
 import axios from 'axios';
 
@@ -7,64 +7,67 @@ function Todo() {
     const [newTodo, setNewTodo] = useState('');
 
     useEffect(() => {
-     fetchTodos();
+        fetchTodos();
     }, []);
 
-    const fetchTodos = () =>{
-      axios.get('http://localhost:8000/todos/')
-      .then((response)=>setTodos(response.data))
-      .catch((err)=>console.log("Error fetching TODOs: ",err));
-
-      console.log(todos);
+    const fetchTodos = () => {
+        axios.get('http://localhost:8000/todos/')
+            .then((response) => setTodos(response.data))
+            .catch((err) => console.error("Error fetching TODOs: ", err));
     }
 
     const handleTodoChange = (event) => {
         setNewTodo(event.target.value);
-      };
+    };
 
-    const handleAddTodo = (e) =>{
+    const handleAddTodo = (e) => {
         e.preventDefault();
-        axios.post('http://localhost:8000/todos/',{description: newTodo})
-        .then((response)=>{
-          alert(response.status === 200? 'Successfully Todo created': 'Error occured')
-            setNewTodo('');
-            fetchTodos();
-        })
-        .catch((err)=>console.log('Error creating TODO: ',err));
+        axios.post('http://localhost:8000/todos/', { description: newTodo })
+            .then((response) => {
+                if (response.status === 201) {
+                    alert('Successfully Todo created');
+                    setNewTodo('');
+                    fetchTodos();
+                } else {
+                    alert('Error occurred');
+                }
+            })
+            .catch((err) => console.error('Error creating TODO: ', err));
     }
-  return (
-    <div className='Todo'>
-        <div>
-        <h1>List of TODOs</h1>
-        {todos.length > 0 ? (
-          <ul>
-            {todos.map((todo) => (
-              <li key={todo._id}>{todo.description}</li>
-            ))}
-          </ul>
-        ) : (
-          <p>No TODOs found.</p>
-        )}
-      </div>
-        <div>
-        <h1>Create a ToDo</h1>
-        <form onSubmit={handleAddTodo}>
-          <div>
-            <label htmlFor="todo">ToDo: </label>
-            <input
-              type="text"
-              id="todo"
-              value={newTodo}
-              onChange={handleTodoChange}
-            />
-          </div>
-          <div style={{ marginTop: '5px' }}>
-            <button type="submit">Add ToDo!</button>
-          </div>
-        </form>
-      </div>
-      </div>
-  );
+
+    return (
+        <div className='Todo'>
+            <div>
+                <h1>List of TODOs</h1>
+                {todos.length > 0 ? (
+                    <ul>
+                        {todos.map((todo) => (
+                            <li key={todo._id}>{todo.description}</li>
+                        ))}
+                    </ul>
+                ) : (
+                    <p>No TODOs found.</p>
+                )}
+            </div>
+            <div>
+                <h1>Create a ToDo</h1>
+                <form onSubmit={handleAddTodo}>
+                    <div>
+                        <label htmlFor="todo">ToDo: </label>
+                        <input
+                            type="text"
+                            id="todo"
+                            value={newTodo}
+                            onChange={handleTodoChange}
+                        />
+                    </div>
+                    <div style={{ marginTop: '5px' }}>
+                        <button type="submit">Add ToDo!</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
 };
 
-export default Todo;    
+export default Todo;
--- src/rest/rest/views.py
+++ src/rest/rest/views.py
@@ -2,28 +2,48 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-import json, logging, os
+import logging
+import os
 from pymongo import MongoClient
+from pymongo.errors import ConnectionFailure
 
-mongo_uri = 'mongodb://' + os.environ["MONGO_HOST"] + ':' + os.environ["MONGO_PORT"]
-# db = MongoClient("mongodb+srv://" + os.environ["CLUSTER_STRING"])['test_db']
-db = MongoClient(mongo_uri)['test_db']
-collection_name = db['adbrew_test']
+
+try:
+    # mongo_uri = 'mongodb://' + os.environ["MONGO_HOST"] + ':' + os.environ["MONGO_PORT"]
+    # db = MongoClient("mongodb+srv://" + os.environ["CLUSTER_STRING"])['test_db']
+    db = MongoClient("mongodb+srv://harshk7134:Harsh%40123@cluster0.uxt0o0d.mongodb.net/bag")['test_db']
+    # db = MongoClient(mongo_uri)['test_db']
+    collection_name = db['adbrew_test']
+except ConnectionFailure as e:
+    logging.error(f'MongoDB connection error: {e}')
+    collection_name = None
 
 class TodoListView(APIView):
-
     def get(self, request):
-        # Implement this method - return all todo items from db instance above.
-        todo_items = list(collection_name.find({}, {'description': 1}))
-        todo_items = [{'_id': str(todo['_id']), 'description': todo['description']} for todo in todo_items]
-        return Response(todo_items, status=status.HTTP_200_OK)
-        
+        if collection_name is None:
+            return Response({'error': 'MongoDB connection error'}, status=status.HTTP_503_SERVICE_UNAVAILABLE)
+        try:
+            todo_items = list(collection_name.find({}, {'description': 1}))
+            todo_items = [{'_id': str(
+                todo['_id']), 'description': todo['description']} for todo in todo_items]
+            return Response(todo_items, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
     def post(self, request):
-        # Implement this method - accept a todo item in a mongo collection, persist it using db instance above.
-        new_todo = {'description': request.data.get('description')}
-        result = collection_name.insert_one(new_todo)
-        response_data = {
-            '_id': str(result.inserted_id),
-            'message': 'TODO created successfully',
-        }
-        return Response(response_data, status=status.HTTP_200_OK)
+        try:
+            if collection_name is None:
+                return Response({'error': 'MongoDB connection error'}, status=status.HTTP_503_SERVICE_UNAVAILABLE)
+
+            new_todo = {'description': request.data.get('description')}
+            result = collection_name.insert_one(new_todo)
+            if result.acknowledged:
+                response_data = {
+                    '_id': str(result.inserted_id),
+                    'message': 'TODO created successfully',
+                }
+                return Response(response_data, status=status.HTTP_201_CREATED)
+            else:
+                return Response({'error': 'Insertion not acknowledged'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        except Exception as e:
+            return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
