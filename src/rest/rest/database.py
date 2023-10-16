408c5d4abc2300153b18ef651a92330bc35af1f4
Gyutaro <80272391+Doma-byte@users.noreply.github.com>
2023-10-16 19:35:20 +0530

    Update database.py

--- src/rest/rest/database.py
+++ src/rest/rest/database.py
@@ -5,17 +5,25 @@ from pymongo.errors import ConnectionFailure
 
 class IDatabaseManager(ABC):
     @abstractmethod
+    def connect(self):
+        pass
+
+    @abstractmethod
     def get_collection(self):
         pass
 
 class DatabaseManager(IDatabaseManager):
     def __init__(self, db_uri):
+        self.db = None
+        self.collection_name = None
+        self.db_uri=db_uri
+
+    def connect(self):
         try:
-            self.db = MongoClient(db_uri)['test_db']
+            self.db = MongoClient(self.db_uri)['test_db']
             self.collection_name = self.db['adbrew_test']
         except ConnectionFailure as e:
             logging.error(f'MongoDB connection error: {e}')
-            self.collection_name = None
 
     def get_collection(self):
         return self.collection_name
