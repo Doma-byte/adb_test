3876cceb2a46a558d60527b0b699567c8b15a1c6
Gyutaro <80272391+Doma-byte@users.noreply.github.com>
2023-10-16 19:03:19 +0530

    Update views.py

--- src/rest/rest/views.py
+++ src/rest/rest/views.py
@@ -1,39 +1,34 @@
+import os
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-import logging
-import os
-from pymongo import MongoClient
-from pymongo.errors import ConnectionFailure
 
-
-try:
-    # mongo_uri = 'mongodb://' + os.environ["MONGO_HOST"] + ':' + os.environ["MONGO_PORT"]
-    db = MongoClient("mongodb+srv://" + os.environ["CLUSTER_STRING"])['test_db']
-    # db = MongoClient(mongo_uri)['test_db']
-    collection_name = db['adbrew_test']
-except ConnectionFailure as e:
-    logging.error(f'MongoDB connection error: {e}')
-    collection_name = None
+from .database import DatabaseManager
 
 class TodoListView(APIView):
+    def __init__(self, db_uri='mongodb://' + os.environ["MONGO_HOST"] + ':' + os.environ["MONGO_PORT"]):
+        super().__init__()
+        self.db_manager = DatabaseManager(db_uri)
+
     def get(self, request):
+        collection_name = self.db_manager.get_collection()
         if collection_name is None:
             return Response({'error': 'MongoDB connection error'}, status=status.HTTP_503_SERVICE_UNAVAILABLE)
+
         try:
             todo_items = list(collection_name.find({}, {'description': 1}))
-            todo_items = [{'_id': str(
-                todo['_id']), 'description': todo['description']} for todo in todo_items]
+            todo_items = [{'_id': str(todo['_id']), 'description': todo['description']} for todo in todo_items]
             return Response(todo_items, status=status.HTTP_200_OK)
         except Exception as e:
             return Response({'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
     def post(self, request):
-        try:
-            if collection_name is None:
-                return Response({'error': 'MongoDB connection error'}, status=status.HTTP_503_SERVICE_UNAVAILABLE)
+        collection_name = self.db_manager.get_collection()
+        if collection_name is None:
+            return Response({'error': 'MongoDB connection error'}, status=status.HTTP_503_SERVICE_UNAVAILABLE)
 
+        try:
             new_todo = {'description': request.data.get('description')}
             result = collection_name.insert_one(new_todo)
             if result.acknowledged:
